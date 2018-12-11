8e78878e182d24ed21df8aedcad17ddf6e3ce1b4
user <user@LAPTOP-583CDFDH>
2018-12-11 16:48:58 +0800

    try

--- src/com/exam/Order.java
+++ src/com/exam/Order.java
@@ -3,17 +3,10 @@ package com.exam;
 import java.util.ArrayList;
 
 public class Order {
-	
-	ArrayList order = new ArrayList();
-	public Order(){
-		String[]c ={"1","2","3","4","5"};
-		order.add(c);
-    }
-	  public void print(){
+	 public void print(){
 		  System.out.println();
 }
 	public void add(Ticket ticket) {
-		// TODO Auto-generated method stub
-		ticket(t);
+		
 	}
 }
--- src/com/exam/Ticket.java
+++ src/com/exam/Ticket.java
@@ -1,7 +1,18 @@
 package com.exam;
 
 public class Ticket {
-	public Ticket(String string, String string2, int a, int b) {
-		
+	public Ticket(String string, String string2, String a, String b) {
+		for (int i = 0; i < string.length(); i++) {
+			char letter = string.charAt(i);
+			System.out.print(letter); }
+		for (int i = 0; i < string2.length(); i++) {
+			char letter = string2.charAt(i);
+			System.out.print(letter); }
+		for (int i = 0; i < a.length(); i++) {
+			char letter = a.charAt(i);
+			System.out.print(letter); }
+		for (int i = 0; i < b.length(); i++) {
+			char letter = b.charAt(i);
+			System.out.print(letter); }
 }
 }
