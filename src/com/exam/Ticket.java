71b963ce46c8e1ad3f8202f89df0a744fae9b412
user <user@LAPTOP-583CDFDH>
2018-12-11 16:54:17 +0800

    finish

--- src/com/exam/Ticket.java
+++ src/com/exam/Ticket.java
@@ -2,17 +2,23 @@ package com.exam;
 
 public class Ticket {
 	public Ticket(String string, String string2, String a, String b) {
+		System.out.println("Result :");
 		for (int i = 0; i < string.length(); i++) {
 			char letter = string.charAt(i);
 			System.out.print(letter); }
+		    System.out.print(" ");
 		for (int i = 0; i < string2.length(); i++) {
 			char letter = string2.charAt(i);
 			System.out.print(letter); }
+			System.out.print(" ");
 		for (int i = 0; i < a.length(); i++) {
 			char letter = a.charAt(i);
 			System.out.print(letter); }
+			System.out.print(" ");
 		for (int i = 0; i < b.length(); i++) {
 			char letter = b.charAt(i);
 			System.out.print(letter); }
+			System.out.print(" ");
+		System.out.println(" ");
 }
 }
